4854bde9fb980544ee1117bacc4aa21a04c5f950
ijon <itan@student.42kl.edu.my>
2023-09-18 23:13:44 +0800

    done ex07

--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -1,4 +1,5 @@
 def ft_filter(function, list):
+    """filter function"""
     if function is None:
         return [i for i in list if i is True]
     return [i for i in list if function(i) is True]
---
+++ ex07/sos.py
@@ -0,0 +1,64 @@
+import sys
+
+
+def morse_code(s):
+    """Convert string to morse code"""
+    morse = {
+        "A": ".-",
+        "B": "-...",
+        "C": "-.-.",
+        "D": "-..",
+        "E": ".",
+        "F": "..-.",
+        "G": "--.",
+        "H": "....",
+        "I": "..",
+        "J": ".---",
+        "K": "-.-",
+        "L": ".-..",
+        "M": "--",
+        "N": "-.",
+        "O": "---",
+        "P": ".--.",
+        "Q": "--.-",
+        "R": ".-.",
+        "S": "...",
+        "T": "-",
+        "U": "..-",
+        "V": "...-",
+        "W": ".--",
+        "X": "-..-",
+        "Y": "-.--",
+        "Z": "--..",
+        "0": "-----",
+        "1": ".----",
+        "2": "..---",
+        "3": "...--",
+        "4": "....-",
+        "5": ".....",
+        "6": "-....",
+        "7": "--...",
+        "8": "---..",
+        "9": "----.",
+        " ": "/"
+    }
+    str = ""
+    for c in s:
+        assert c.isalnum() or c.isspace(), "string must be alphanumeric"
+        if c.upper() in morse:
+            str += morse[c.upper()] + " "
+    print(str)
+
+
+def main():
+    """main function"""
+    try:
+        assert len(sys.argv) == 2, "One arguments only!!!"
+        morse_code(sys.argv[1])
+    except AssertionError as e:
+        print("AssertionError: ", e)
+    return None
+
+
+if __name__ == "__main__":
+    main()
