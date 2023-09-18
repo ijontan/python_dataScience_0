0af43b0dee01605bfafef6e0b0c0150f2cb21e62
ijon <itan@student.42kl.edu.my>
2023-09-18 16:31:24 +0800

    ex00

---
+++ ex00/Hello.py
@@ -0,0 +1,17 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+ft_list[1] = "World"
+ft_tuple = ("Hello","Malaysia")
+ft_set.remove("tutu!")
+tmp = ft_set.pop()
+ft_set.add(tmp)
+ft_set.add("Selangor")
+ft_dict["Hello"] = "42KL"
+#your code here
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
