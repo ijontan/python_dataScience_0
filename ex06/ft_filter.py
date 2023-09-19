365237de2fd4b7214893c3a79cf0cf21a2a5f99d
ijon <itan@student.42kl.edu.my>
2023-09-19 18:22:24 +0800

    correct doc

--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -1,5 +1,8 @@
 def ft_filter(function, list):
-    """filter function"""
+    """filter(function or None, iterable) --> filter object
+
+Return an iterator yielding those items of iterable for which function(item)
+is true. If function is None, return the items that are true."""
     if function is None:
         return [i for i in list if i is True]
     return [i for i in list if function(i) is True]
